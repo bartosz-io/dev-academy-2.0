a288fba0df8013a52d79c1d901907e48d556e8af
Łukasz Fajger <lukasfajger@gmail.com>
2021-08-07 21:32:18 +0200

    Add subscribe tag

---
+++ themes/my-theme/scripts/subscribe.js
@@ -0,0 +1,11 @@
+hexo.extend.tag.register('subscribe', function () {
+    return `<div class="custom-subscribe">
+                <div class="custom-subscribe-content">
+                    <img src="/img/ninja-jump.png" width="200" height="160" alt="" loading="lazy">
+                    <div class="custom-subscribe-text">
+                        <p>Did you like the content?</p>
+                        <a class="button button-primary" data-formkit-toggle="e4bf864ac2" href="https://dev-academy.ck.page/e4bf864ac2">Subscribe for MORE!</a>
+                    </div>
+                </div>
+            </div>`;
+});
