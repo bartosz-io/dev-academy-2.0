8b0d6236f9842d0b61ef53301e263467de963e1f
Łukasz Fajger <lukasfajger@gmail.com>
2022-09-21 20:51:26 +0200

    Add related post styles

--- themes/my-theme/source/js/main.js
+++ themes/my-theme/source/js/main.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function() {
     cookieConsent();
     loadDisqusComments();
     loadConvertKit();
-    // relatedPosts();
+    relatedPosts();
     authors();
 });
 
