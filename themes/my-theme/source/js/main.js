86f2c68655c33d3c0ee0991000050f095ab5ae1c
bartosz-io <pietrucha.bartosz@gmail.com>
2023-01-29 15:27:51 +0100

    Use cookie bot

--- themes/my-theme/source/js/main.js
+++ themes/my-theme/source/js/main.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
         setActiveTagPill();
     }
 
-    cookieConsent();
+    // cookieConsent();
     loadDisqusComments();
     loadConvertKit();
     relatedPosts();
