90f522a092e9e15876252035f6ae9a3dbfab95cd
Lukasz Fajger <lukasfajger@gmail.com>
2023-03-04 10:35:38 +0100

    feat(testing) Add A/B testing for main header

--- themes/my-theme/source/js/posthog/testing/dumb-test.js
+++ themes/my-theme/source/js/posthog/testing/dumb-test.js
@@ -2,6 +2,7 @@ if (window.posthog) {
     posthog.onFeatureFlags(function() {
         // window.DESKTOP_STICKY_HEADER = true;
         sidebarWSALinkABTest();
+        mainBannerLinkABTest();
     });
 }
 
@@ -9,7 +10,6 @@ function sidebarWSALinkABTest() {
     var articleSidebar = document.querySelector('.article .article-sidebar');
 
     if (posthog.getFeatureFlag('blog-post-sidebar_wsa') === 'test') {
-        console.warn('Starting test flag');
 
         if (articleSidebar) {
             articleSidebar.classList.add('article-sidebar--dark');
@@ -19,6 +19,27 @@ function sidebarWSALinkABTest() {
     if (articleSidebar) {
         articleSidebar.classList.remove('hidden');
     }
+}
 
-    console.warn('End testing');
+function mainBannerLinkABTest() {
+    if (posthog.getFeatureFlag('main-banner_link') === 'test') {
+        var barTopClass = 'b-bar-top';
+        var barBottomClass = 'b-bar-bottom';
+        var mainBanner = document.querySelector('.main-banner');
+
+        if (document.body.classList.contains(barTopClass) && mainBanner) {
+            var firstText = mainBanner.querySelector("[data-copy='1']");
+            var middleText = mainBanner.querySelector("[data-copy='2']");
+            var textLink = mainBanner.querySelector("[data-copy='3']");
+
+            if (firstText && middleText && textLink) {
+                // firstText.textContent = '<<TO BE ADDED IF NEEDED>'
+                middleText.textContent = '🔥 FAST LEARNING 🔥'
+                textLink.textContent = 'CHECK THIS OUT'
+            }
+
+            document.body.classList.remove(barTopClass);
+            document.body.classList.add(barBottomClass);
+        }
+    }
 }
