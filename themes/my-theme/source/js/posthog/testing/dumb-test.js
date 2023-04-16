15cb0c15e57ad0ce5cf6333fcb463c820ac3f1d2
bartosz-io <pietrucha.bartosz@gmail.com>
2023-04-16 14:16:23 +0200

    Disable test

--- themes/my-theme/source/js/posthog/testing/dumb-test.js
+++ themes/my-theme/source/js/posthog/testing/dumb-test.js
@@ -1,7 +1,7 @@
 if (window.posthog) {
     posthog.onFeatureFlags(function() {
         // window.DESKTOP_STICKY_HEADER = true;
-        sidebarWSALinkABTest();
+        // sidebarWSALinkABTest();
         // mainBannerLinkABTest();
     });
 }
