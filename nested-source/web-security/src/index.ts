d9e7b1f908abcfd30b4eca79aa81fc942884b41b
bartosz-io <pietrucha.bartosz@gmail.com>
2023-10-02 22:19:15 +0200

    Disable CK on WSDA

--- nested-source/web-security/src/index.ts
+++ nested-source/web-security/src/index.ts
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     moreTestimonials();
-    loadConvertKit();
+    // loadConvertKit();
     // loadTawk();
     // collapsePanel();
     startTimer();
